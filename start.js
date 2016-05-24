beaf53babd35ce4871bfc5d5ea01a4af86b8e9fe
Gede Suparsa <suparsag@global.anz.com>
2016-05-24 22:56:11 +1000

    fixing port for redshift

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 var atlasboard = require('atlasboard');
-atlasboard({port: process.env.ATLASBOARD_PORT || 3000, install: true}, function (err) {
+atlasboard({port: process.env.ATLASBOARD_PORT || 8080, install: true}, function (err) {
   if (err) {
     throw err;
   }
